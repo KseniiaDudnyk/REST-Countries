c8cf99d4ed95c6b615fabc2118e2ab5d6efe50a4
Kseniia Dudunyk <kseniia.dudnyk@innovecs.com>
2019-05-17 11:58:26 +0300

    Get-data component created

--- apps/myworkspace/src/app/app.module.ts
+++ apps/myworkspace/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from "./app.component";
 
 import { StoreModule } from '@ngrx/store';
 import { getData } from './app.reducer';
+import { GetDataComponent } from './get-data/get-data.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GetDataComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({ data: getData })
