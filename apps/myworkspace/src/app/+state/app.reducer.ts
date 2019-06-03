a08d11e4abbb45557de1caf8b5d80702b60d91cf
Kseniia <51192893+KseniiaDudnyk@users.noreply.github.com>
2019-06-03 17:40:57 +0300

    Delete useless comments.

--- apps/myworkspace/src/app/+state/app.reducer.ts
+++ apps/myworkspace/src/app/+state/app.reducer.ts
@@ -3,10 +3,10 @@ import { Country } from '../countries.interface';
 export const APP_FEATURE_KEY = 'app';
 
 export interface AppState {
-  list: Country[]; // list of App; analogous to a sql normalized table
-  selectedId?: string | number; // which App record has been selected
-  loaded: boolean; // has the App list been loaded
-  error?: any; // last none error (if any)
+  list: Country[];
+  selectedId?: string | number;
+  loaded: boolean;
+  error?: any;
 }
 
 export interface AppPartialState {
